e8684d2c5c773916f79341f9b83ef81bd8e37f5e
Rishi Bansal <rishibansal02@gmail.com>
2022-08-22 09:47:30 +0530

    .

--- src/design/pattern/User.java
+++ src/design/pattern/User.java
@@ -1,5 +1,6 @@
 package design.pattern;
 
+// To Demostrate Builder
 public class User
 {
     //All final attributes
@@ -76,23 +77,23 @@ public class User
     }
     public static void main(String[] args)
     {
-        User user1 = new User.UserBuilder("Lokesh", "Gupta")
-                .age(30)
-                .phone("1234567")
-                .address("Fake address 1234")
+        User user1 = new User.UserBuilder("Sunil", "Gupta")
+                .age(35)
+                .phone("2348762373")
+                .address("Some address 34862")
                 .build();
 
         System.out.println(user1);
 
-        User user2 = new User.UserBuilder("Jack", "Reacher")
-                .age(40)
-                .phone("5655")
+        User user2 = new User.UserBuilder("Tom", "Cruise")
+                .age(25)
+                .phone("2342543")
                 //no address
                 .build();
 
         System.out.println(user2);
 
-        User user3 = new User.UserBuilder("Super", "Man")
+        User user3 = new User.UserBuilder("Tulsi", "Kumar")
                 //No age
                 //No phone
                 //no address
