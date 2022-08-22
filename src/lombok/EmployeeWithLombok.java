5c62c2d6276bf274db3872769d8e1020b4a4fb53
Rishi Bansal <rishibansal02@gmail.com>
2022-08-22 10:26:09 +0530

    .

--- src/lombok/EmployeeWithLombok.java
+++ src/lombok/EmployeeWithLombok.java
@@ -1,17 +1,17 @@
 package lombok;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
 public class EmployeeWithLombok {
-    private @Getter @Setter Integer employeeId;
-    private @Getter @Setter String name;
-    private @Getter @Setter String company;
-    private @Getter @Setter String emailId;
+  //  private @Getter @Setter Integer employeeId;
+    //private @Getter @Setter String name;
+    //private @Getter @Setter String company;
+    //private @Getter @Setter String emailId;
 }
